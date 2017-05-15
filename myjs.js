092ebc4bc63f337d25cc0e9f19bba935fea086c6
gdeschapelles <gonzalo.deschapelles@greenwichschools.org>
2017-05-14 21:21:43 -0400

    Add files via upload

--- myjs.js
+++ myjs.js
@@ -7,7 +7,7 @@
 	  var token = result.credential.accessToken;
 	  // The signed-in user info.
 	  var user = result.user;
-	  console.log(user);
+	  console.log(user.val());
 	  // ...
 	}).catch(function(error) {
 	  // Handle Errors here.
